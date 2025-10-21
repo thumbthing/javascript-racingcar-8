8b7a25683c9f3d3303b1cf190a9e130582215714
thumbthing <ilscc0902@gmail.com>
2025-10-22 03:02:25 +0900

    feat(parse): 시도 횟수 변환 기능-상세 목록 추가
    - 클래스 내 메서드 추가
    - 기능 설명 추가

--- src/parse/ParseInput.js
+++ src/parse/ParseInput.js
@@ -3,4 +3,9 @@ export class ParseInput {
     const CAR_LIST = input.split(',');
     return CAR_LIST;
   }
+
+  getAttempts(input) {
+    const ATTEMPT_COUNT = Number(input);
+    return ATTEMPT_COUNT;
+  }
 }
