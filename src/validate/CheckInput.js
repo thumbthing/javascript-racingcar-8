9b8485cf5d86bc569500f2a4f360f98ac9ba9d9d
thumbthing <ilscc0902@gmail.com>
2025-10-23 22:49:51 +0900

    feat(checkInput): 자동차 이름 입력 유효성 통과 조건 추가
    유효성 통과 조건
    - 배열 길이 2 이상
    - 중복되지 않은 자동차 이름으로 구성
    
    유효성 검사 상수 이름 구체화
    - 조건 추가에 의거한 상수명 구체화
    - 검사된 항목들의 통합 상수 추가

--- src/validate/CheckInput.js
+++ src/validate/CheckInput.js
@@ -1,8 +1,11 @@
 export class CheckInput {
   carName(carList) {
-    const IS_VALID = carList.every((car) => {
+    const IS_VALID_LENGTH = carList.length > 1;
+    const IS_VALID_CAR_NAME = carList.every((car) => {
       return car.length <= 5 && car.length > 0;
     });
+    const IS_UNIQUE_CAR_NAME = carList.length === new Set(carList).size;
+    const IS_VALID = IS_VALID_LENGTH && IS_VALID_CAR_NAME && IS_UNIQUE_CAR_NAME;
 
     if (IS_VALID === false) {
       throw new Error("[ERROR]");
