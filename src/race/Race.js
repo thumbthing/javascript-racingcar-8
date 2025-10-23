7817675d71134b895db707136be0578af1c245f3
thumbthing <ilscc0902@gmail.com>
2025-10-24 02:03:17 +0900

    feat(race): 레이스 기록 필드-최신화 기능 추가
    필드
    - 생성자로 Map 형태의 자료구조로 초기화
    
    레이스 기록 최신화
    - 자동차 이름 필드를 순회
    - getCondition() 메서드로 최신화 상태 생성
    - 레이스 기록 필드에 레이스 위치 최신화

--- src/race/Race.js
+++ src/race/Race.js
@@ -5,11 +5,26 @@ export class Race {
   constructor(carList, attemptCount) {
     this.carList = carList;
     this.attemptCount = attemptCount;
+    this.raceRecord = new Map(carList.map((car) => {
+      return [car, ""];
+    }));
   }
-  
+
+  // 전진 조건 생성
   getCondition() {
     const RANDOM_NUMBER = Random.pickNumberInRange(0, 9);
     const IS_OVER_FOUR = RANDOM_NUMBER >= 4;
     return IS_OVER_FOUR;
+  }
+
+  // 레이스 기록 최신화
+  updateRecord() {
+    this.carList.forEach((car) => {
+      const CONDITION = this.getCondition();
+      if (CONDITION) {
+        const UPDATED_POSITION = this.raceRecord.get(car) + "-"
+        this.raceRecord.set(car, UPDATED_POSITION);
+      } 
+    })
   }
 }
