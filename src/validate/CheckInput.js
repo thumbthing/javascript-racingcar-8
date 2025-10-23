caf2d5ea34366715779a81e93eb2ec03de2d3283
thumbthing <ilscc0902@gmail.com>
2025-10-23 21:26:26 +0900

    feat(checkInput): 시도 횟수 입력값 검사 기능 추가
    - CheckInput 클래스 내 메서드 추가
    - NaN, 0 이하, 소수 케이스 판단

--- src/validate/CheckInput.js
+++ src/validate/CheckInput.js
@@ -8,4 +8,14 @@ export class CheckInput {
       throw new Error("[ERROR]");
     }
   }
+
+  attemptCount(attemptCount) {
+    const IS_NaN = Number.isNaN(attemptCount);
+    const IS_TOO_SMALL = attemptCount <= 0;
+    const IS_DICIMAL = Number.isInteger(attemptCount);
+
+    if (IS_NaN || IS_TOO_SMALL || IS_DICIMAL) {
+      throw new Error("[ERROR]");
+    }
+  }
 }
