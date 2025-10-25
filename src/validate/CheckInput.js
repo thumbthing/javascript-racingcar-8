2690c2e5c3485e7341c305b747ed869ca9adcdbf
thumbthing <ilscc0902@gmail.com>
2025-10-26 05:25:06 +0900

    feat(checkInput): 에러 처리 기능 확장
    추가 사항
    - 전역 상수 : INPUT_ERROR => 에러코드, 메시지 데이터
    - 초기 필드 : errorStatus
    - 추가된 메서드 : initializeStatus(), updateErrorStatusByCarList(), updateErrorStatusByAttempt()
    - 기존 메서드 내부 구조 변경

--- src/validate/CheckInput.js
+++ src/validate/CheckInput.js
@@ -1,25 +1,64 @@
+import { Console } from "@woowacourse/mission-utils";
+
+const INPUT_ERROR = {
+  "lessThenTwoCar" : "레이스할 자동차가 부족합니다. 최소 2대 이상 입력해주세요",
+  "inCorrectLength": "입력하신 자동차의 이름중에 5자를 초과 또는 비어있습니다. 5자 이하의 자동차 이름을 입력해주세요",
+  "notUniqueCarName": "중복된 자동차 이름이 존재합니다",
+  "notNumber": "시도하실 횟수가 숫자가 아닙니다. 숫자를 입력해주세요",
+  "tooSmall": "시도하실 횟수가 0 이하입니다. 1 이상의 숫자를 입력해주세요",
+  "decimal": "시도하실 횟수가 소수입니다. 양의 정수를 입력해주세요"
+}
+
 export class CheckInput {
+  errorStatus = new Map([
+    ["lessThenTwoCar",false],
+    ["inCorrectLength",false],
+    ["notUniqueCarName",false],
+    ["notNumber",false],
+    ["tooSmall",false],
+    ["decimal",false],
+  ])
+
+  intializeStatus() {
+    const errorKey = [...this.errorStatus.keys()]
+    errorKey.forEach((errorName) => this.errorStatus.set(errorName, false));
+  }
+
+  updateErrorStatusByCarList(carList) {
+    if (carList.length < 2) this.errorStatus.set("lessThenTwoCar", true);
+    if (carList.some((car) => { return car.length >= 6 || car.length <= 0})) this.errorStatus.set("inCorrectLength", true);
+    if (carList.length > new Set(carList).size) this.errorStatus.set("notUniqueCarName", true);
+  }
+
+  updateErrorStatusByAttempt(attemptCount) {
+    if (Number.isNaN(attemptCount)) this.errorStatus.set("notNumber", true);
+    if (attemptCount <= 0) this.errorStatus.set("tooSmall", true);
+    if (Math.floor(attemptCount) !== attemptCount || Math.ceil(attemptCount) !== attemptCount) this.errorStatus.set("decimal", true);
+  }
 
   carList(carList) {
-    const isLessThenTwoCar = carList.length < 2;
-    const isCarNameTooLong = carList.some((car) => {
-      return car.length >= 6 || car.length === 0;
-    });
-    const isNotUniqueCarName = carList.length > new Set(carList).size;
-    const isInValid = isLessThenTwoCar || isCarNameTooLong || isNotUniqueCarName;
+    const errorKey = [...this.errorStatus.keys()];
+    this.updateErrorStatusByCarList(carList);
 
-    if (isInValid) {
-      throw new Error("[ERROR]");
-    }
+    [...this.errorStatus.values()].some((errorStatus, index) => {
+      if (errorStatus) {
+        Console.print(INPUT_ERROR[errorKey[index]]);
+        this.intializeStatus();
+        throw new Error("[ERROR]");
+      }
+    })
   }
 
   attemptCount(attemptCount) {
-    const isNotNumber = Number.isNaN(attemptCount);
-    const isTooSmall = attemptCount <= 0;
-    const isDicimal = Math.floor(attemptCount) !== attemptCount || Math.ceil(attemptCount) !== attemptCount;
+    const errorKey = [...this.errorStatus.keys()];
+    this.updateErrorStatusByAttempt(attemptCount);
 
-    if (isNotNumber || isTooSmall || isDicimal) {
-      throw new Error("[ERROR]");
-    }
+    [...this.errorStatus.values()].some((errorStatus, index) => {
+      if (errorStatus) {
+        Console.print(INPUT_ERROR[errorKey[index]]);
+        this.intializeStatus();
+        throw new Error("[ERROR]");
+      }
+    })
   }
 }
