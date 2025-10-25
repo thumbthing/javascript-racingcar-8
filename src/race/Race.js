6b4635edd8b8ef2ab7a08397c24844258db89d7e
thumbthing <ilscc0902@gmail.com>
2025-10-26 01:35:02 +0900

    style(Race): 매겨변수 구체화, 지역 변수 컨벤션 수정
    - 지역 변수 SNAKE_CASE => camel case로 변경
    - 함수의 매개변수 구체화

--- src/race/Race.js
+++ src/race/Race.js
@@ -12,54 +12,54 @@ export class Race {
 
   // 전진 조건 생성
   getCondition() {
-    const RANDOM_NUMBER = Random.pickNumberInRange(0, 9);
-    const IS_OVER_FOUR = RANDOM_NUMBER >= 4;
-    return IS_OVER_FOUR;
+    const randomNumber = Random.pickNumberInRange(0, 9);
+    const isOverFour = randomNumber >= 4;
+    return isOverFour;
   }
 
   // 레이스 기록 최신화
   updateRecord() {
     this.carList.forEach((car) => {
-      const CONDITION = this.getCondition();
-      if (CONDITION) {
-        const UPDATED_POSITION = this.raceRecord.get(car) + "-"
-        this.raceRecord.set(car, UPDATED_POSITION);
+      const isOverFour = this.getCondition();
+      if (isOverFour) {
+        const updatedPosition = this.raceRecord.get(car) + "-"
+        this.raceRecord.set(car, updatedPosition);
       } 
     })
   }
 
   // 결과 문자열 생성
   createRecordString() {
-    const RECORD = this.carList.map((car) => {
+    const record = this.carList.map((car) => {
       return `${car} : ${this.raceRecord.get(car)}`
     })
-    const RECORD_STRING = `${RECORD.join('\n')}\n`;
-    return RECORD_STRING;
+    const recordString = `${record.join('\n')}\n`;
+    return recordString;
   }
 
   // 우승자 위치 반환
   getWinnerPosition() {
-    const RECORD_POSITION = [...this.raceRecord.values()].map((record) => record.length);
-    const WINNER_RECORD = Math.max(...RECORD_POSITION);
-    return WINNER_RECORD;
+    const recordPosition = [...this.raceRecord.values()].map((record) => record.length);
+    const winnerPosition = Math.max(...recordPosition);
+    return winnerPosition;
   }
 
   // 우승자 명단 생성
-  getWinnerList(winnerRecord) {
-    const WINNER = [];
+  getWinnerList(winnerPosition) {
+    const winner = [];
     this.carList.forEach((car) => {
-      const RECORD = this.raceRecord.get(car);
-      if (RECORD.length === winnerRecord) WINNER.push(car);
+      const record = this.raceRecord.get(car);
+      if (record.length === winnerPosition) winner.push(car);
     });
-    return WINNER;
+    return winner;
   }
 
   // 우승자 선별
   getWinner() {
-    const MAX_RECORD = this.getWinnerPosition();
-    const WINNER = this.getWinnerList(MAX_RECORD);
-    const RESULT_STRING = `최종 우승자 : ${WINNER.join(', ')}`;
-    Console.print(RESULT_STRING);
+    const winnerPosition = this.getWinnerPosition();
+    const winner = this.getWinnerList(winnerPosition);
+    const resultString = `최종 우승자 : ${winner.join(', ')}`;
+    Console.print(resultString);
   }
 
   // 시도할 횟수 만큼 반복
@@ -68,8 +68,8 @@ export class Race {
 
     for (let count = 0; count < this.attemptCount; count++) {
       this.updateRecord()
-      const RECORD_STRING = this.createRecordString();
-      Console.print(RECORD_STRING);
+      const recordString = this.createRecordString();
+      Console.print(recordString);
     }
 
     this.getWinner();
