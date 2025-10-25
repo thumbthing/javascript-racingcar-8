1220b72be6bdf69d76c43e2a36affaa1638e9ae4
thumbthing <ilscc0902@gmail.com>
2025-10-26 03:08:56 +0900

    refactor(app): 각 기능 별 모듈화
    기능에 따른 모듈화
    - 인스턴스화
    - 자동차 이름, 시도횟수
    - 실행

--- src/App.js
+++ src/App.js
@@ -4,26 +4,48 @@ import { Race } from "./race/Race.js";
 import { CheckInput } from "./validate/CheckInput.js";
 
 class App {
-  async run() {
-    // 기능 인스턴스화
+
+  // 필요 기능 인스턴스화
+  initialize() {
     const userInput = new UserInput();
     const parser = new ParseInput();
     const checkValidate = new CheckInput();
+    return {userInput, parser, checkValidate};
+  }
 
-    // 입력 -> 변환 -> 검증
-    // 자동차 이름
+  // 자동차 이름
+  // 입력 -> 변환 -> 검증 -> 변환값 반환
+  async getCarNameFromUserInput(userInput, parser, checkValidate) {
     const carInput = await userInput.getCarName();
     const carList = parser.parseCarList(carInput);
     checkValidate.carList(carList);
+    return carList;
+  }
 
-    // 시도 횟수
+  // 시도 횟수
+  // 입력 -> 변환 -> 검증 -> 변환값 반환
+  async getAttemptCountFromUserInput(userInput, parser, checkValidate) {
     const attemptInput = await userInput.getRaceCount();
     const attemptCount = parser.parseAttempts(attemptInput);
     checkValidate.attemptCount(attemptCount);
+    return attemptCount;
+  }
 
-    // 레이스 실행
+  // 레이스 실행
+  startRace(carList, attemptCount) {
     const race = new Race(carList, attemptCount);
     race.run();
+  }
+
+  async run() {
+    // 기능 인스턴스화
+    const { userInput, parser, checkValidate } = this.initialize();
+
+    const carList = await this.getCarNameFromUserInput(userInput, parser, checkValidate);
+    const attemptCount = await this.getAttemptCountFromUserInput(userInput, parser, checkValidate);
+
+    // 레이스 실행
+    this.startRace(carList, attemptCount);
   }
 }
 
