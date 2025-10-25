efb7d35740adc53aceb9ba8221ced980f39ad899
thumbthing <ilscc0902@gmail.com>
2025-10-26 01:14:13 +0900

    style(UserInput): 메서드명, 지역 변수 컨벤션 수정
    - getInput 메서드명 구체화
    - 지역변수 SNAKE_CASE => camel case 로 변경

--- src/input/Input.js
+++ src/input/Input.js
@@ -1,20 +1,20 @@
 import { Console } from "@woowacourse/mission-utils";
 
 export class UserInput {
-  async getInput(notice) {
-    const INPUT = Console.readLineAsync(`${notice}\n`);
-    return INPUT;
+  async getUserInput(notice) {
+    const input = Console.readLineAsync(`${notice}\n`);
+    return input;
   }
 
   async getCarName() {
-    const NOTICE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
-    const INPUT = await this.getInput(NOTICE);
-    return INPUT;
+    const notice = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
+    const input = await this.getUserInput(notice);
+    return input;
   }
 
   async getRaceCount() {
-    const NOTICE = "시도할 횟수는 몇 회인가요?";
-    const INPUT = await this.getInput(NOTICE);
-    return INPUT;
+    const notice = "시도할 횟수는 몇 회인가요?";
+    const input = await this.getUserInput(notice);
+    return input;
   }
 }
